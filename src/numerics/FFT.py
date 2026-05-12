977ce694df330b4cbfd8f9715aa7aba74f35be05
Devtropy <hectoracostaherrera@gmail.com>
2026-05-11 19:11:01 -0700

    limpieza

--- src/numerics/FFT.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-
-def FFT3D(Campo):
-    return np.fft.fftn(Campo)
-
-
-def IFFT3D(Campo):
-    return np.fft.ifftn(Campo).real
--- src/numerics/variables_aleatorias.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-
-def Fase():
-    return np.random.uniform(0, 2 * np.pi)
-
-
-def Rayleigh(sigma):
-    return np.random.rayleigh(sigma)
