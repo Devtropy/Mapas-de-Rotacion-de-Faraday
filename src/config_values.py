9c788980c9d9e575401156e0e46dbf7003c4f658
Devtropy <hectoracostaherrera@gmail.com>
2026-06-13 22:52:53 -0700

    hotfix5

--- src/config_values.py
+++ src/config_values.py
@@ -1,13 +1,13 @@
 import config as cfg_units
-import astropy.units as u 
+import astropy.units as u
 
 # Variables adimensionales
-N_BASE = cfg_units.N_BASE 
+N_BASE = cfg_units.N_BASE
 N_REFINADO = cfg_units.N_REFINADO
-BETA = cfg_units.BETA 
+BETA = cfg_units.BETA
 MU = cfg_units.MU
-N_SPEC = cfg_units.N_SPEC 
-P_SPEC = cfg_units.P_SPEC 
+N_SPEC = cfg_units.N_SPEC
+P_SPEC = cfg_units.P_SPEC
 
 # Extracción de valores en unidades necesarias para la matmática
 DX_BASE_KPC = cfg_units.DX_BASE.to(u.kpc).value
@@ -20,9 +20,9 @@ B0_MG = cfg_units.B0.to(u.microgauss).value
 
 LAMBDA_MIN_KPC = cfg_units.LAMBDA_MIN.to(u.kpc).value
 LAMBDA_MAX_KPC = cfg_units.LAMBDA_MAX.to(u.kpc).value
-
+BEAM_FWHM_KPC = cfg_units.BEAM_FWHM.to(u.kpc).value
 NU_HZ = cfg_units.NU.to(u.Hz).value
 LAMBDA_ONDA_M = cfg_units.LAMBDA_ONDA.to(u.m).value
 
-#dx en pc para el cálculo de RM
+# dx en pc para el cálculo de RM
 DX_BASE_PC = cfg_units.DX_BASE.to(u.pc).value
