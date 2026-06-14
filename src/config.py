664b904057cc71dcd5e6fd4a89cdc603f2c8c0c4
Devtropy <hectoracostaherrera@gmail.com>
2026-06-13 22:49:20 -0700

    hotfix4

--- src/config.py
+++ src/config.py
@@ -1,29 +1,28 @@
-import astropy.units as u 
+import astropy.units as u
 
-#para el observatorio
+# para el observatorio
 BEAM_FWHM_KPC = 50.0
-=======
 # Parametros para la malla
-N_BASE = 128                        # Adimensional           
-N_REFINADO = 256                    # Adimensional
-DX_BASE = 10.0*u.kpc                # tamaño de pixel 
-DX_REFINADO = 3.0*u.kpc             # tamaño de pixel 
-RADIO_REFINAMIENTO = 200.0*u.kpc    # radio 
+N_BASE = 128  # Adimensional
+N_REFINADO = 256  # Adimensional
+DX_BASE = 10.0 * u.kpc  # tamaño de pixel
+DX_REFINADO = 3.0 * u.kpc  # tamaño de pixel
+RADIO_REFINAMIENTO = 200.0 * u.kpc  # radio
 
 # Parametros del medio intrecúmulo (ICM)
-N0 = 1e-3*(u.cm**-3)                # Densidad central de electrones 
-RC = 400.0*u.kpc                    # Radio del core  
-BETA = 0.6                          # Parametro beta (adimensional)
-B0 = 1.0*u.microgauss               # Intensidad magnetica 
-MU = 0.5                            # Fraccion molecular (adimensional)
+N0 = 1e-3 * (u.cm**-3)  # Densidad central de electrones
+RC = 400.0 * u.kpc  # Radio del core
+BETA = 0.6  # Parametro beta (adimensional)
+B0 = 1.0 * u.microgauss  # Intensidad magnetica
+MU = 0.5  # Fraccion molecular (adimensional)
 
 # Parametros de turbulencia
-LAMBDA_MIN = 6.0*u.kpc              # Escala mínima de disipación 
-LAMBDA_MAX = 768.0*u.kpc            # Escala máxima de inyección 
-N_SPEC = 3.0                        # Índice del espectro magnético 
-P_SPEC = 3.0                        # Índice de energía de electrones relativistas
+LAMBDA_MIN = 6.0 * u.kpc  # Escala mínima de disipación
+LAMBDA_MAX = 768.0 * u.kpc  # Escala máxima de inyección
+N_SPEC = 3.0  # Índice del espectro magnético
+P_SPEC = 3.0  # Índice de energía de electrones relativistas
 
 # Parametros de observación
-NU = 1.4e9*u.Hz                     # Frecuencia de observación 
-C = 3e8*(u.m/u.s)                   # Velocidad de la luz 
-LAMBDA_ONDA = C / NU                # Longitud de onda 
+NU = 1.4e9 * u.Hz  # Frecuencia de observación
+C = 3e8 * (u.m / u.s)  # Velocidad de la luz
+LAMBDA_ONDA = C / NU  # Longitud de onda
